5491c8ec09b957b1609c1b77a57721f99bdc62df
shrice <32008203+shrice@users.noreply.github.com>
2018-03-11 17:32:21 +0900

    add views

--- src/cljs/catalog/core.cljs
+++ src/cljs/catalog/core.cljs
@@ -1,12 +1,14 @@
 (ns catalog.core
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
-            [catalog.views :as views]))
+            [catalog.views :as views]
+            [catalog.events :as events]))
 
 (defn render!
   []
   (rf/clear-subscription-cache!)
-  (r/render [(views/pages :home)]
+  (rf/dispatch [::events/init-db])
+  (r/render [(views/page)]
             (js/document.getElementById "app")))
 
 (defn ^:export init
